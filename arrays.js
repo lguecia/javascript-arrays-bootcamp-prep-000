18ea5790427a357cda652a82e32be5d48154cab7
Leah Guecia <lguecia@gmail.com>
2018-10-15 16:57:18 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,7 +5,7 @@ var ingredient4 = "butter";
 var ingredient5 = "tomato";
 var ingredient6 = "garlic";
 
-var chocolateBars = new Array("snickers", "hundred grand", "kitkat", "skittles");
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function.chocolateBars() {
   return
