518878258e6250043f3aaab0b1a95cca542b9e3b
Leah Guecia <lguecia@gmail.com>
2018-10-15 17:37:08 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -19,7 +19,7 @@ function destructivelyAddElementToEndOfArray(array, element) {
 }
 
 function destrutivelyRemoveElementFromBeginningOfArray(array) {
-  array.shift(1);
+  array.shift();
   array;
 }
 
