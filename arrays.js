5e60542951bb94945a8ea8103c00831d2830f0b8
Leah Guecia <lguecia@gmail.com>
2018-10-15 17:02:05 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -7,7 +7,7 @@ var ingredient6 = "garlic";
 
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-function addElementToBeginningOfArray(chocolateBars)
+function addElementToBeginningOfArray([0])
 
 function destructivelyAddElementToBeginningOfArray()
 
